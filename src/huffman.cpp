01977c2f9e787cb89f5f51f5e3f11920e47a1790
Artem Melnyk <melkam464@gmail.com>
2023-05-17 22:53:49 +0200

    fix

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -1,106 +1,92 @@
+#include "huffman.hpp"
+
 #include <iostream>
 #include <string>
 #include <queue>
 #include <unordered_map>
 #include <memory>
+#include <utility>
 
 namespace Huffman
 {
-  struct TreeNode
+  bool TreeNode::Compare::operator()(
+      std::shared_ptr<TreeNode> left,
+      std::shared_ptr<TreeNode> right)
   {
-  private:
-    struct Compare
-    {
-      bool operator()(std::shared_ptr<TreeNode> left, std::shared_ptr<TreeNode> right)
-      {
-        return left->frequency > right->frequency;
-      }
-    };
-
-  public:
-    char symbol;
-    unsigned frequency;
-    std::shared_ptr<TreeNode> left;
-    std::shared_ptr<TreeNode> right;
-
-    TreeNode(char symbol, unsigned frequency)
-        : symbol(symbol), frequency(frequency) {}
-
-    static std::shared_ptr<TreeNode> buildTree(const std::string &text)
-    {
-      std::unordered_map<char, unsigned> frequencyTable;
-      for (char c : text)
-      {
-        ++frequencyTable[c];
-      }
-
-      std::priority_queue<std::shared_ptr<TreeNode>, std::vector<std::shared_ptr<TreeNode>>, Compare> queue;
-
-      for (const auto &[symbol, frequency] : frequencyTable)
-      {
-        queue.push(std::make_shared<TreeNode>(symbol, frequency));
-      }
-
-      while (queue.size() > 1)
-      {
-        auto left = queue.top();
-        queue.pop();
-
-        auto right = queue.top();
-        queue.pop();
-
-        auto newNode = std::make_shared<TreeNode>('$', left->frequency + right->frequency);
-        newNode->left = left;
-        newNode->right = right;
-
-        queue.push(newNode);
-      }
-
-      return queue.top();
-    }
+    return left->frequency > right->frequency;
   };
 
-  class EncodingMap
+  TreeNode::TreeNode(char symbol, unsigned frequency)
+      : symbol(symbol), frequency(frequency) {}
+
+  std::shared_ptr<TreeNode> TreeNode::from(
+      const std::string &text)
   {
-  private:
-    std::unordered_map<char, std::string> map;
-
-    void build(std::shared_ptr<Huffman::TreeNode> node, std::string code)
+    std::unordered_map<char, unsigned> frequencyTable;
+    for (char c : text)
     {
-      if (node.get() == nullptr)
-        return;
-
-      if (node->left == nullptr && node->right == nullptr)
-      {
-        map[node->symbol] = code;
-        return;
-      }
-
-      build(node->left, code + "0");
-      build(node->right, code + "1");
+      ++frequencyTable[c];
     }
 
-  public:
-    EncodingMap(const std::string &text)
+    std::priority_queue<std::shared_ptr<TreeNode>, std::vector<std::shared_ptr<TreeNode>>, TreeNode::Compare> queue;
+
+    for (const auto &[symbol, frequency] : frequencyTable)
     {
-      auto root = TreeNode::buildTree(text);
-      build(root, "");
+      queue.push(std::make_shared<TreeNode>(symbol, frequency));
     }
 
-    const std::string &operator[](char symbol)
+    while (queue.size() > 1)
     {
-      return map[symbol];
+      auto left = queue.top();
+      queue.pop();
+
+      auto right = queue.top();
+      queue.pop();
+
+      auto newNode = std::make_shared<TreeNode>('$', left->frequency + right->frequency);
+      newNode->left = left;
+      newNode->right = right;
+
+      queue.push(newNode);
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const EncodingMap &map)
+    return queue.top();
+  }
+
+  // recursive method
+  void EncodingMap::build(std::shared_ptr<TreeNode> node, const std::string &code = "")
+  {
+    if (node.get() == nullptr)
+      return;
+
+    if (node->left == nullptr && node->right == nullptr)
     {
-      for (auto &&[key, value] : map.map)
-      {
-        std::cout << key << "=" << value << std::endl;
-      }
-      return os;
+      map[node->symbol] = code;
+      return;
     }
-  };
+
+    build(node->left, code + "0");
+    build(node->right, code + "1");
+  }
+
+  EncodingMap::EncodingMap(const std::string &text)
+  {
+    build(TreeNode::from(text));
+  }
+
+  const std::string &EncodingMap::operator[](char symbol)
+  {
+    return map[symbol];
+  }
+
+  std::ostream &operator<<(std::ostream &os, const EncodingMap &map)
+  {
+    for (auto &&[key, value] : map.map)
+    {
+      std::cout << key << "=" << value << std::endl;
+    }
+    return os;
+  }
 
   std::pair<std::string, EncodingMap> encode(const std::string &text)
   {
--- src/huffman.hpp
+++ src/huffman.hpp
@@ -1,17 +1,16 @@
-#ifndef HUFFMAN_H
-#define HUFFMAN_H
+#pragma once
 
 #include <iostream>
 #include <string>
 #include <queue>
 #include <unordered_map>
 #include <memory>
+#include <utility>
 
 namespace Huffman
 {
   struct TreeNode
   {
-  public:
     char symbol;
     unsigned frequency;
     std::shared_ptr<TreeNode> left;
@@ -19,20 +18,31 @@ namespace Huffman
 
     TreeNode(char symbol, unsigned frequency);
 
-    static std::shared_ptr<TreeNode> buildTree(const std::string &text);
+    struct Compare
+    {
+      bool operator()(std::shared_ptr<Huffman::TreeNode> left, std::shared_ptr<Huffman::TreeNode> right);
+    };
+
+    static std::shared_ptr<TreeNode> from(const std::string &text);
+    // static std::shared_ptr<TreeNode> from(const EncodingMap &map);
   };
 
   class EncodingMap
   {
+  private:
+    std::unordered_map<char, std::string> map;
+
+    void build(std::shared_ptr<TreeNode> node, const std::string &code);
+
   public:
     EncodingMap(const std::string &text);
 
     const std::string &operator[](char symbol);
 
     friend std::ostream &operator<<(std::ostream &os, const EncodingMap &map);
+
+    static EncodingMap from(const std::string &str_map);
   };
 
   std::pair<std::string, EncodingMap> encode(const std::string &text);
 }
-
-#endif /* HUFFMAN_H */
